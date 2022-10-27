f600d51cd66c301be8a7b2ff1437494e58d87645
Justin Solo <justinpaulosolo@gmail.com>
2022-10-26 18:58:56 -0700

    About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,13 @@
 export default function About() {
   return (
-    <div>
-      <h1>About me</h1>
+    <div className="flex flex-col mx-auto max-w-3xl">
+      <h1 className="font-bold text-4xl">About</h1>
+      <p className="pt-2">
+        Software Engineer experienced in full-stack development with a focus in
+        back-end development. I love building interactive tools that are
+        innovative, simple, and accessible.
+      </p>
+      <h3 className="font-bold text-2xl pt-2">On the web</h3>
     </div>
   );
 }
